0730f9e8682dab83b95caf02cded2f327767742d
Sven Trajbar <sven.trajbar33@gmail.com>
2022-11-26 13:26:40 +0100

    Check the mod description

--- src/main/java/net/sven/testmod/item/ModItems.java
+++ src/main/java/net/sven/testmod/item/ModItems.java
@@ -16,6 +16,44 @@ public class ModItems {
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TEST_TAB)));
     public static final RegistryObject<Item> SILVER_INGOT = ITEMS.register("silver_ingot",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TEST_TAB)));
+
+    public static final RegistryObject<Item> RAW_TIN = ITEMS.register("raw_tin",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TEST_TAB)));
+
+    public static final RegistryObject<Item> TIN_INGOT = ITEMS.register("tin_ingot",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TEST_TAB)));
+
+    public static final RegistryObject<Item> RAW_BRONZE = ITEMS.register("raw_bronze",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TEST_TAB)));
+
+    public static final RegistryObject<Item> BRONZE_INGOT = ITEMS.register("bronze_ingot",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TEST_TAB)));
+
+    public static final RegistryObject<Item> RAW_PLATINUM = ITEMS.register("raw_platinum",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TEST_TAB)));
+
+    public static final RegistryObject<Item> PLATINUM_INGOT = ITEMS.register("platinum_ingot",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TEST_TAB)));
+
+
+    public static final RegistryObject<Item> GYPSUM_DUST = ITEMS.register("gypsum_dust",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TEST_TAB)));
+
+    public static final RegistryObject<Item> SILICEOUS_ROCK = ITEMS.register("siliceous_rock",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TEST_TAB)));
+
+
+    public static final RegistryObject<Item> CAROB_SEEDS = ITEMS.register("carob_seeds",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TEST_TAB)));
+
+    public static final RegistryObject<Item> OLIVE = ITEMS.register("olive",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TEST_TAB)));
+
+
+
+
+    public static final RegistryObject<Item> TEST_ITEM = ITEMS.register("test_item",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TEST_TAB)));
     public static void refister(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
